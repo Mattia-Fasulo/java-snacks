67c705f2bf401671e90e4e207d5e2b2483c617c5
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-03-09 14:17:59 +0100

    add snack 1

---
+++ src/org/snacks/java/Snack1.java
@@ -0,0 +1,27 @@
+package org.snacks.java;
+
+import java.util.Scanner;
+
+public class Snack1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num;
+
+        //faccio inserire un numero all'utente
+        System.out.print("Inserisci un numero: ");
+        num = Integer.parseInt(scanner.nextLine());
+
+        System.out.println();
+
+        if (num % 2 == 0) { // se il numero è pari lo stampo
+            System.out.println(num);
+        } else { //altrimenti incremento il numero di uno e lo stampo
+            num++;
+            System.out.println(num);
+        }
+
+    }
+
+}
